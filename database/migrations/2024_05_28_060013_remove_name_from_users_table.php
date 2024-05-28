1d405659baec233514dc7990f21ce2e16f9e918b
Max <kuzminitb@gmail.com>
2024-05-28 12:03:49 +0600

    change users table

--- database/migrations/2024_05_28_060013_remove_name_from_users_table.php
+++ database/migrations/2024_05_28_060013_remove_name_from_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnsToUsersTable extends Migration
+class RemoveNameFromUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,7 @@ class AddColumnsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('first_name')->nullable()->after('id');
-            $table->string('second_name')->nullable()->after('first_name');
-            $table->string('company_name')->after('second_name');
-            $table->string('position')->after('company_name');
+            $table->dropColumn('name');
         });
     }
 
@@ -29,10 +26,7 @@ class AddColumnsToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('first_name');
-            $table->dropColumn('second_name');
-            $table->dropColumn('company_name');
-            $table->dropColumn('position');
+            $table->string('name');
         });
     }
 }
